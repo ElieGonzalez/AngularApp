8bcb710033c0444aa735a81bfd97f06d31147972
elgonzalez <elgonzalez@indec.gob.ar>
2024-02-15 12:11:36 -0300

    feat: add shyft service and pages

---
+++ src/app/home-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-app-home-page',
+  template: `<h2>Home</h2>`,
+  standalone: true,
+})
+export class HomePageComponent {}
---
+++ src/app/settings-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-app-setting-page',
+  template: `<h2>Settings</h2>`,
+  standalone: true,
+})
+export class SettingsPageComponent {}
