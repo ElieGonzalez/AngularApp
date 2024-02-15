3b8c0bf1f545971790701def707b5723972bb2be
elgonzalez <elgonzalez@indec.gob.ar>
2024-02-15 13:17:49 -0300

    Add hero and feactures section

---
+++ src/app/hero-section.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-app-hero-section',
+  template: ` 
+  <section class="px-24 py-32 bg-white bg-opacity-5">
+    <p class="text-center text-3xl">
+      This is the Hero
+    </p>
+  </section>
+  `,
+  standalone: true,
+})
+export class HeroSectionComponent {}
--- src/app/home-page.component.ts
+++ src/app/home-page.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from './hero-section.component';
+import { FeacturesSectionComponent } from './feactures-section.component';
 
 @Component({
   selector: 'angular-app-home-page',
-  template: `<h2>Home</h2>`,
+  template: `
+  <angular-app-hero-section></angular-app-hero-section>
+  <angular-app-feactures-section></angular-app-feactures-section>
+  `,
   standalone: true,
+  imports: [HeroSectionComponent, FeacturesSectionComponent]
 })
 export class HomePageComponent {}
